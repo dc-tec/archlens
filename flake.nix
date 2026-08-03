a2a121864f852df27d0738f29afb9c6edf593807
Roel de Cort <roel@decort.tech>
2026-08-04 01:01:57 +0200

    chore(release): start 0.1.1 development
    Signed-off-by: Roel de Cort <roel@decort.tech>

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
           };
           archlens = pkgs.vimUtils.buildVimPlugin {
             pname = "archlens.nvim";
-            version = "0.1.0";
+            version = "0.1.1-dev";
             src = archlensSource;
             meta = {
               description = "Human-first architectural relationship lens for Neovim";
